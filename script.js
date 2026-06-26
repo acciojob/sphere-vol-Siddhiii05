5eb8d6407f5b41e0a67c25156e706612234b3360
Siddhiii05 <siddhi.bobade05@gmail.com>
2026-06-26 19:54:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-function volume_sphere() {
-    //Write your code here
-	event.preventDefault();
-	const radius = parseFloat(document.getElementById("radius").value);
-	const volume = document.getElememtById("volume");
+function volume_sphere(event) {
+    event.preventDefault();
 
-	if(isNaN(radius)||radius < 0){
-		volume.value = NaN;
-	} else {
-		const result = (4/3) * Math.PI * Math.pow(radius, 3);
-		volume.value = result.toFixed(4);
-	}
-} 
+    const radius = parseFloat(document.getElementById("radius").value);
+    const volume = document.getElementById("volume");
+
+    if (isNaN(radius) || radius < 0) {
+        volume.value = NaN;
+    } else {
+        volume.value = ((4 / 3) * Math.PI * radius ** 3).toFixed(4);
+    }
+}
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
